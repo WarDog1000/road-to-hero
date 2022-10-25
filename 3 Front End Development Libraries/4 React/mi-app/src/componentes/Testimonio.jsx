91d2defaec601b0849755b451cbbbf3552e8aac2
Jhony Souza <jhonatansouzameza100@gmail.com>
2022-10-25 13:10:09 -0300

    Update mi-app/App.js

--- 3 Front End Development Libraries/4 React/mi-app/src/componentes/Testimonio.jsx
+++ 3 Front End Development Libraries/4 React/mi-app/src/componentes/Testimonio.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../estilos/Testimonio.css';
 export function Testimonio(props) {
-    return (
-        <div className="contenedor-testimonio">
-            <img src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt="Soto de Emma" className="imagen-testimonio" />
-            <div className="contenedor-texto-testimonio">
-                <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-                <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
-                <p className="texto-testimonio">"{props.testimonio}"</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="contenedor-testimonio">
+      <img src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt='' className="imagen-testimonio" />
+      <div className="contenedor-texto-testimonio">
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  )
 }
 
 export default Testimonio;
