c60badde27124d137421749b3b6cb426cc54775c
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-04-02 06:13:24 -0300

    add reloj function

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/2_relog/reloj.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/2_relog/reloj.js
@@ -1,1 +1,8 @@
 /* **********     Curso JavaScript: 82. DOM: Ejercicios Prácticos | Reloj Digital - #jonmircha     ********** */
+export function digitalClock(clock, btnPlay, btnStop) {
+
+}
+
+export function alarm() {
+
+}
--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/index-dom.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/index-dom.js
@@ -1,4 +1,8 @@
 import hamburgerBtn from "./components/1_hamburger_btn/hamburger_btn.js"
+import {digitalClock, alarm} from "./components/2_reloj/reloj.js"
 
 const d = document
-d.addEventListener("DOMContentLoaded", e => hamburgerBtn('.hamburger', '.panel', '.menu a'))
+d.addEventListener("DOMContentLoaded", e => {
+  hamburgerBtn('.hamburger', '.panel', '.menu a')
+  digitalClock('#reloj', '#')
+} )
