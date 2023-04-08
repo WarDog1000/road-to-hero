3ca819c2b0fc0dadf91198e7ecf0058a86534d5b
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-04-08 04:02:13 -0300

    add responsive design javascript

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/7_responsive-design/responsive-design.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/7_responsive-design/responsive-design.js
@@ -1,1 +1,6 @@
 /* **********     Curso JavaScript: 90. DOM: Ejercicios Prácticos | Responsive con JavaScript - #jonmircha     ********** */
+const d = document
+w =window
+export default function responsiveMedia(id, mediaQuery, mobileConstent, desktopContent) {
+  
+}
