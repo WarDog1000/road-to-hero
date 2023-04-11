abb7adb4051831c0f0a78b91923a62113a104d05
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-04-11 04:27:29 -0300

    add responsive tester

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/8_responsive-tester/responsive-tester.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/8_responsive-tester/responsive-tester.js
@@ -6,7 +6,17 @@ export default function responsiveTester(idForm) {
   d.addEventListener('submit', e => {
     if(e.target === $form) {
       e.preventDefault()
-      
+      tester = window.open(
+        $form.direccion.value,
+        'tester',
+        `innerWidth=${$form.ancho.value}, innerHeight=${$form.alto.value}`
+      )
     }
+  })
+
+  d.addEventListener('click', e => {
+    if(e.target === $form.cerrar) {
+      tester.close()
+    } 
   })
 }
