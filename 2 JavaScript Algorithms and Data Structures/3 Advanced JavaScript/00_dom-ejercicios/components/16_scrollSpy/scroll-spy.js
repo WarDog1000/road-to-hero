01119ca4fa3892804f1d7b068138e3962d2b0b03
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-04-18 06:21:08 -0300

    add scroll spy function

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/16_scrollSpy/scroll-spy.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/16_scrollSpy/scroll-spy.js
@@ -1,8 +1,21 @@
 /* **********     Curso JavaScript: 99. DOM: Ejercicios Prácticos | ScrollSpy con Intersection Observer - #jonmircha     ********** */
 const d = document
 export default function scrollSpy() {
+
   const $sections = d.querySelectorAll('section[data-scroll-spy]')
-  const cb = entries => entries.forEach( entry => entry)
-  const observer = new IntersectionObserver(cd, {})
-  $sections.forEach( le => observer(el))
+
+  // funcion callback
+  const cb = entries => entries.forEach( entry => {
+    const id = entry.target.getAttribute('id'),
+    $anchorMenu = d.querySelector(`a[data-scroll-spy][href="#${id}"]`)
+    (entry.isIntersecting)
+    ? $anchorMenu.classList.add('active')
+    : $anchorMenu.classList.remove('active')
+  })
+
+  const observer = new IntersectionObserver(cb, {
+    // root,
+    rootMargin: '-250px' // margenes negativos reducen el rango de observacion
+  })
+  $sections.forEach( el => observer.observe(el))
 }
