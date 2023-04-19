9f5d07add7a4161f080a656da3ca2892b327c810
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-04-19 04:50:22 -0300

    apply IntersectionObservern function

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/16_scrollSpy/scroll-spy.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/00_dom-ejercicios/components/16_scrollSpy/scroll-spy.js
@@ -1,21 +1,34 @@
 /* **********     Curso JavaScript: 99. DOM: Ejercicios Prácticos | ScrollSpy con Intersection Observer - #jonmircha     ********** */
 const d = document
 export default function scrollSpy() {
+  // los entries son los elementos que entran o interactuan con el campo de visualizacion
+  const callbak = (entries) => {
+    // console.log('entradas ', entries)
 
-  const $sections = d.querySelectorAll('section[data-scroll-spy]')
+    // por cada entrada ejecutara la siguiente funcion
+    entries.forEach( entrada => {
+      // console.log('entrada ',entrada)
 
-  // funcion callback
-  const cb = entries => entries.forEach( entry => {
-    const id = entry.target.getAttribute('id'),
-    $anchorMenu = d.querySelector(`a[data-scroll-spy][href="#${id}"]`)
-    (entry.isIntersecting)
-    ? $anchorMenu.classList.add('active')
-    : $anchorMenu.classList.remove('active')
+      const id = entrada.target.getAttribute('id'),
+      $anchorMenu = d.querySelector(`a[data-scroll-spy][href="#${id}"]`)
+      if(entrada.isIntersecting) {
+        $anchorMenu.classList.add('active')
+      } else {
+       $anchorMenu.classList.remove('active')
+      }
+    })
+    
+  },
+  $sections = d.querySelectorAll('section[data-scroll-spy]') // selecciona todos los elementos con el atributo data-scroll-spy
+
+  // se crea un observador, el cual recibe una funcion callbak pla cual podra manejar las observaciones
+  const observador = new IntersectionObserver(callbak, { // margenes de observacion
+    // root
+    // rootMargin: '-250px',
+    threshold: [0.5, 0.75], // min max 50% / 75% de visibilidad
   })
+  console.log('Observador', observador)
 
-  const observer = new IntersectionObserver(cb, {
-    // root,
-    rootMargin: '-250px' // margenes negativos reducen el rango de observacion
-  })
-  $sections.forEach( el => observer.observe(el))
+  // recorre el conjunto de $sections y asigna el observador a cada elemento dentro del conjunto
+  $sections.forEach( el => observador.observe(el) )
 }
