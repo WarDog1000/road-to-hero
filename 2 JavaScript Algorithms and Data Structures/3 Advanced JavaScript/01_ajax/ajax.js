5fe3e009147d26396716755f8e5613c5625057ef
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-04-23 03:41:17 -0300

    add ajax folder

---
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/01_ajax/ajax.js
@@ -0,0 +1,13 @@
+/* **********     Curso JavaScript: 106. AJAX: Objeto XMLHttpRequest - #jonmircha     ********** */
+
+
+/* **********     Curso JavaScript: 107. AJAX: API Fetch - #jonmircha     ********** */
+
+
+/* **********     Curso JavaScript: 108. AJAX: API Fetch + Async-Await - #jonmircha     ********** */
+
+
+/* **********     Curso JavaScript: 109. AJAX: Librería Axios - #jonmircha     ********** */
+
+
+/* **********     Curso JavaScript: 110. AJAX: Librería Axios + Async-Await - #jonmircha     ********** */
