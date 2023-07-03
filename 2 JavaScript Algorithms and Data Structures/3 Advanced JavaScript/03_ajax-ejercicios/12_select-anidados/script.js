d6d9c50abb1965d465510509981385c689ae3e44
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-07-03 02:35:32 -0300

    learn api key & auth

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/03_ajax-ejercicios/12_select-anidados/script.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/03_ajax-ejercicios/12_select-anidados/script.js
@@ -1,14 +1,36 @@
 const d = document,
 selectPrimary = d.getElementById("select-primary"),
-selectSecondary = d.getElementById("select-secondary")
+selectSecondary = d.getElementById("select-secondary"),
+selectCities = d.getElementById("select-city")
 
-function loadStates() {
-  fetch("https://api")
+let auth = ""
+
+function autentication() {
+  fetch("https://www.universal-tutorial.com/api/getaccesstoken", {
+    "Accept": "application/json",
+    "api-token": "LE_k6HSfIy3fcOJX4exPX3YVxrlrIUadBESAazB-jdKtbHC8njCqtss2ucLgOh-7Omk",
+    "user-email": "jhonatansouzameza100@gmail.com"
+  })
+  .then( res => res.ok ? res.json() : Promise.reject(res))
+  .then( json => {
+    auth = json.auth_token
+  })
+  .catch( err => console.log(err))
+}
+
+
+function loadCountries() {
+  fetch("https://www.universal-tutorial.com/api/countries", {
+    "Authorization": `Bearer ${auth}`,
+    "Accept": "application/json"
+  })
   .then( res => res.ok ? res.json() : Promise.reject(res))
   .then( json => {
     // console.log(json)
-    let options = `<option value="">Elije un Estado</option>`
-    json.response.estado.forEach( el => options += `<option value="${el}">${el}</option>`)
+    let options = `<option value="">Elije un Pais</option>`
+    json.forEach( item => {
+     options += `<option value="${item.country_name}">${item.country_name}</option>`;
+    })
     selectPrimary.innerHTML = options
   })
   .catch(err => {
@@ -17,13 +39,18 @@ function loadStates() {
     selectPrimary.nextElementSibling.innerHTML = `Error ${err.status}: ${message}`
   })
 }
-function loadTowns(state) {
-  fetch(`https://api${state}`)
+function loadStates(country) {
+  fetch(`https://www.universal-tutorial.com/api/countries/states/${country}`, {
+    "Authorization": `Bearer ${auth}`,
+    "Accept": "application/json"
+  })
   .then( res => res.ok ? res.json() : Promise.reject(res))
   .then( json => {
     // console.log(json)
-    let options = `<option value="">Elije un Municipio</option>`
-    json.response.municipios.forEach( el => options += `<option value="${el}">${el}</option>`)
+    let options = `<option value="">Elije un Estado</option>`
+    json.forEach( item => {
+     options += `<option value="${item.state_name}">${item.state_name}</option>`;
+    })
     selectSecondary.innerHTML = options
   })
   .catch( err => {
@@ -33,5 +60,28 @@ function loadTowns(state) {
   })
 }
 
-d.addEventListener('DOMContentLoaded', e => loadStates)
-selectPrimary.addEventListener('change', e => loadTowns(e.target.value))
+function loadCities(state) {
+  fetch(`https://www.universal-tutorial.com/api/cities/${state}`, {
+    "Authorization": `Bearer ${auth}`,
+    "Accept": "application/json"
+  })
+  .then( res => res.ok ? res.json() : Promise.reject(res))
+  .then( json => {
+    // console.log(json)
+    let options = `<option value="">Elije una Ciudad</option>`
+    json.forEach( item => {
+     options += `<option value="${item.city_name}">${item.city_name}</option>`;
+    })
+    selectCities.innerHTML = options
+  })
+  .catch( err => {
+    console.log(err)
+    let message = err.statusText || "Ocurrio un error!"
+    selectCities.nextElementSibling.innerHTML = `Error ${err.status}: ${message}`
+  })
+
+}
+
+d.addEventListener('DOMContentLoaded', e => loadCountries)
+selectPrimary.addEventListener('change', e => loadStates(e.target.value))
+selectSecondary.addEventListener('change', e => loadCities(e.target.value))
