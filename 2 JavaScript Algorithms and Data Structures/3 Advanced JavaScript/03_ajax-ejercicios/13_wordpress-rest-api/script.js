fb7e30d378cf9dfbd89e5227fef1515adf0b11b4
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-07-07 06:23:23 -0300

    clase 141

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/03_ajax-ejercicios/13_wordpress-rest-api/script.js
+++ 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/03_ajax-ejercicios/13_wordpress-rest-api/script.js
@@ -1,16 +1,15 @@
 const d = document,
-site = d.getElementsByTagName("site"),
-post = d.getElementsByTagName("post"),
+site = d.getElementById("site"),
+posts = d.getElementById("posts"),
 loader = d.querySelector(".loader"),
-template = d.getElementById("post-template").contentEditable,
+template = d.getElementById("post-template").content,
 fragment = d.createDocumentFragment(),
-DOMAIN = "https://css-tricks.com",
+DOMAIN = "https://malvestida.com",
 SITE = `${DOMAIN}/wp-json`,
 API_WP = `${SITE}/wp/v2`,
 POSTS = `${API_WP}/posts`
 PAGES = `${API_WP}/pages`
 CATEGORIES = `${API_WP}/categories`
-
 
 // fetch(POSTS)
 // .then( res => res.ok ? res.json() : Promise.reject(res))
@@ -20,8 +19,14 @@ function  getSiteData() {
   fetch(SITE)
   .then( res => res.ok ? res.json() : Promise.reject(res))
   .then( json => {
-
+    console.log(json)
+    site.innerHTML = `
+    <h3>Sitio Web</h3>
+    <h2><a href="${json.url}" target="_blank">${json.name}</a></h2>
+    <p>${json.description}</p>
+    <p>${json.timezone_string}</p>`
   })
+  .then( () => loader.style.display = "none")
   .catch( err => {
     console.log(err)
     let message = err.statusText || "Ocurrio un error"
@@ -30,15 +35,24 @@ function  getSiteData() {
 }
 
 function getPosts() {
+  loader.style.display = "block"
   fetch(POSTS)
   .then( res => res.ok ? res.json() : Promise.reject(res))
   .then( json => {
-
+    console.log(json)
+    json.forEach( el => {
+      template.querySelector(".post-title").innerHTML = el.title.rendered
+      let clone = d.importNode(template, true)
+      fragment.appendChild(clone)
+    })
+    posts.appendChild(fragment)
+    loader.style.display = "none"
   })
   .catch( err => {
     console.log(err)
     let message = err.statusText || "Ocurrio un error"
-    post.innerHTML = `<p>Error ${err.status}: ${message}</p>`
+    posts.innerHTML = `<p>Error ${err.status}: ${message}</p>`
+    loader.style.display = "none"
   })
 }
 
