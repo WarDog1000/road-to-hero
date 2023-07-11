437b31c5db852c5dc6d5f37d996b058f09c1ba81
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-07-11 02:04:19 -0300

    delete tailwindcss folder

--- 2 JavaScript Algorithms and Data Structures/3 Advanced JavaScript/05_TailwindCss/src/js/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('mobile-menu')
-
-menuBtn.addEventListener('click', () => {
-  const menuLinks = document.getElementById('mobile-links')
-  menuLinks.classList.toggle('hidden')
-})
