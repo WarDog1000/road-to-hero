857bbe36c0c81e7c28d2509137283e932df71fe7
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-09-21 06:22:00 -0300

    add form handlers

--- 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_5/components/ContactForm.jsx
+++ 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_5/components/ContactForm.jsx
@@ -1,35 +1,64 @@
 import React from 'react'
 import { useForm } from '../hooks/useForm';
-const initialForm = {}; // valor inicial del formulario
-const validateForm = (form) => {}; // funcion para validar el formulario
+const initialForm = {
+  name: "",
+  email: "",
+  subject: "",
+  comment: ""
+}; // valor inicial del formulario
+
+const validateForm = (form) => {
+  let errors = {};
+  if(!form.name. trim()) {
+    errors.name = "Name is required";
+  }
+}; // funcion para validar el formulario
 
 function ContactForm() {
   // guarda la des-tructuracion del objeto de datos proveniente de hooks/useForm
   const {form, errloading, res, handleChange, handleBlur, handleSubmit} = useForm(initialForm, validateForm);
+
+  const inputStyles = {
+    border: "thin solid #dedede",
+    borderRadius: "0.25rem",
+    padding: "0.75rem",
+    marginBottom: "1rem",
+    outline: "none",
+    display: "block",
+    width: "100%",
+    fontSize: "1rem",
+    lineHeight: 1,
+    backgroundColor: "transpartent",
+    fontFamily: "inherit", resize: "none" 
+  }
+  
   return (
     <>
-      <h2>contact Form</h2>
+      <h2>Contact Form</h2>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          style={inputStyles}
           name="name"
           value={form.name}
           placeholder = "escribe tu nombre"
           onChange={handleChange}
           onBlur={handleBlur}
           required
-            />
+          />
         <input
           type="email"
+          style={inputStyles}
           name="email"
           value={form.email}
           placeholder = "escribe tu email"
           onChange={handleChange}
           onBlur={handleBlur}
           required
-            />
+          />
         <input
           type="text"
+          style={inputStyles}
           name="subject"
           value={form.subject}
           placeholder = "asunto a tratar"
@@ -37,7 +66,15 @@ function ContactForm() {
           onBlur={handleBlur}
           required
             />
-        <textarea name="comments" cols="50" rows="5" placeholder="escribe tu comentario" onChange={handleChange} onBlur={handleBlur} value={form.comments} ></textarea>
+        <textarea 
+          style={inputStyles}
+          name="comments"
+          value={form.comments}
+          cols="50"
+          rows="5" 
+          placeholder="escribe tu comentario" 
+          onChange={handleChange} onBlur={handleBlur} >
+        </textarea>
         <input type="submit" value="Enviar" />
       </form>
     </>
--- 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_5/hooks/useForm.js
+++ 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_5/hooks/useForm.js
@@ -7,10 +7,19 @@ export function useForm(iniForm, validateForm) {
   const [res, setRes] = useState(null);
 
   // donde ocuurre un cambio en el elemento
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
   
   // donde se pierde el foco de un elemento
-  const handleBlur = (e) => {}
+  const handleBlur = (e) => {
+     handleChange(e);
+     setErr()
+  }
 
   // cuando se envia un formulario
   const handleSubmit = (e) => {}
