c9f41f51e9aec23b7050eb8ce0b09ca454275fcf
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-09-28 04:20:59 -0300

    learn modals in react

--- 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_6/Tarea6.jsx
+++ 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_6/Tarea6.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
+import { useModal } from './hooks/useModal';
 import Modal from './components/Modal';
-import { useModal } from './hooks/usemodal';
 function Tarea6() {
   const [isOpenModal1, openModal1, closeModal1] = useModal(false);
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenModal3, openModal3, closeModal3] = useModal(false);
   return (
     <>
       Tarea 6 - Modals & Childrens Props
-      <button>Modal 1</button>
-      <Modal isOpen={isOpenModal1}>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
         <h3>Modal 1</h3>
         <p>Contenido del modal 1</p>
         <img src="https://loremflickr.com/400/400" alt="placeimg" />
       </Modal>
-      <button>Modal 2</button>
-      <Modal isOpen={isOpenModal2}>
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
         <h3>Modal 2</h3>
         <p>Contenido del modal 2</p>
+        <img src="https://loremflickr.com/400/400" alt="flickr" />
+      </Modal>
+      <button onClick={openModal3}>Modal 3</button>
+      <Modal isOpen={isOpenModal3} closeModal={closeModal3}>
+        <h3>Modal 3</h3>
+        <p>Contenido del modal 3</p>
         <img src="https://loremflickr.com/400/400" alt="flickr" />
       </Modal>
     </>
--- 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_6/components/Modal.jsx
+++ 3 Front End Development Libraries/02_react/02_react-middle/src/ejercicios/tarea_6/components/Modal.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-function Modal({children}) {
+import './Modal.css'
+function Modal({children, isOpen, closeModal}) {
+  // funcion evitar el cierre cunado ocurre un click en el "container" del modal
+  const handleContainerClick = e => e.stopPropagation();
   return (
-    <article className='modal is-open'>
-      <div className="modal-container">
-        <button className="modal-close">X</button>
+    <article className={`modal ${isOpen && "is-open"}`} onClick={closeModal}>
+      <div className="modal-container" onClick={handleContainerClick}>
+        <button className="modal-close" onClick={closeModal}>X</button>
         <>{children}</>
       </div>
     </article>
