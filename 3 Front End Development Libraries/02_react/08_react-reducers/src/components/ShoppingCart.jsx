4c578f9243acbee48dc5feca15ba48bc88eeeaac
WarDog1000 <jhonatansouzameza100@gmail.com>
2023-10-14 08:41:56 -0300

    add shoppingcart actions

---
+++ 3 Front End Development Libraries/02_react/08_react-reducers/src/components/ShoppingCart.jsx
@@ -0,0 +1,13 @@
+function ShoppingCart() {
+  return (
+    <div>
+      <h2>Carrito de Compras con Reducers</h2>
+      <h3>Productos</h3>
+      <article className="box"></article>
+        <h3>Carrirto</h3>
+      <article className="box"></article>
+    </div>
+  );
+}
+
+export default ShoppingCart;
