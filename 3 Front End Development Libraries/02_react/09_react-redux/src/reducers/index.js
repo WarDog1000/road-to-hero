bc11047acf708d89764a5e4a1f7530b47ed82569
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-10-20 06:08:32 -0300

    add contador con redux

--- 3 Front End Development Libraries/02_react/09_react-redux/src/reducers/index.js
+++ 3 Front End Development Libraries/02_react/09_react-redux/src/reducers/index.js
@@ -1,5 +1,8 @@
 import {combineReducers} from "redux";
+import contadorReducer from "./contador.reducer";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  contador: contadorReducer
+});
 
 export default reducer;
