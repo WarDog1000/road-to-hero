4d269c1c4d271589a94d33046dd83a9085f060ba
Jhony Souza <jhonatansouzameza100@gmail.com>
2024-05-22 06:14:23 -0300

    220520240614

--- 1 Responsive Web Design/2 CSS3/tailwindcss/tailwind.config.js
+++ 1 Responsive Web Design/2 CSS3/tailwindcss/tailwind.config.js
@@ -12,8 +12,18 @@ module.exports = {
       },
       width: {
         '43': '170px'
+      },
+      screens: {
+        'talet': '900px'
       }
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    }
   },
   plugins: [],
 }
